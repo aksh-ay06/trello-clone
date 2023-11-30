4cdecb103a712c9d2161668903ee71dba316e984
Akshay <ap7839426@gmail.com>
2023-11-30 13:30:08 +0530

    Completed the initial landing page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,8 +1,13 @@
+import { Footer } from "./_components/footer";
+import { Navber } from "./_components/navbar";
+
 const MarketingLayout = ({children}:{children:React.ReactNode;}) => {
     return (
         <div className="h-full bg-slate-100">
             <main className="pt-40 pb-20 bg-slate-100">
+            <Navber/>
             {children}
+            <Footer/>
             </main>
         </div>
     );
